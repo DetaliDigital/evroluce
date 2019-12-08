2813156e3cbbcebcf102e2c82464e1b35151f281
DetaliDigital <admin@detali.digital>
2019-12-09 01:37:21 +0300

    Добавление полей в оформление заказа

---
+++ core/components/ms2orderaddressfields/msorderaddress.map.inc.php
@@ -0,0 +1,42 @@
+<?php
+return array(
+    'fields' => array(
+        'corp' => NULL,
+        'entrance' => NULL,
+        'floor' => NULL,
+        'code' => NULL,
+        'company' => NULL,
+    ),
+    'fieldMeta' => array(
+        'corp' => array(
+          'dbtype' => 'int',
+          'precision' => '10',
+          'phptype' => 'string',
+          'null' => true,
+        ),
+        'entrance' => array(
+          'dbtype' => 'int',
+          'precision' => '10',
+          'phptype' => 'string',
+          'null' => true,
+        ),
+        'floor' => array(
+          'dbtype' => 'int',
+          'precision' => '10',
+          'phptype' => 'string',
+          'null' => true,
+        ),
+        'code' => array(
+          'dbtype' => 'int',
+          'precision' => '10',
+          'phptype' => 'string',
+          'null' => true,
+        ),
+        'company' => array(
+          'dbtype' => 'varchar',
+          'precision' => '255',
+          'phptype' => 'string',
+          'null' => true,
+        ),
+    ),
+);
